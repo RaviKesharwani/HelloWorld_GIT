f57f6dddec61d24a7792ae2542443b8140082438
RaviKesharwani <42150915+RaviKesharwani@users.noreply.github.com>
2022-04-18 15:46:48 +0530

    Add files via upload

---
+++ helloWorld.java
@@ -0,0 +1,10 @@
+package rough;
+
+public class helloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("============Welcome in Jenkins Hello World Program=============");
+
+	}
+
+}
